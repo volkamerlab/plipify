667f4207184c66452a47b45e98eeec1afd1163b0
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-05-07 11:13:57 +0200

    Fix tests

--- plipify/__init__.py
+++ plipify/__init__.py
@@ -4,11 +4,12 @@ PLIPify project
 """
 
 # Add imports here
-from .test import *
+from .core import *
 
 # Handle versioneer
 from ._version import get_versions
+
 versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
 del get_versions, versions
