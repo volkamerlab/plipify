039ce2aba6ac44dbd61f9b750cd51a7a4053309f
fritzfra <fritzfra@gmail.com>
2020-05-20 17:45:49 +0200

    move table code to external file

--- plipify/fp_visual.py
+++ plipify/fp_visual.py
@@ -7,6 +7,17 @@ from IPython.core.display import display, HTML
 import ipywidgets as widgets
 from ipywidgets import HBox
 
+interaction_colours = {
+    "hydrophobic": "#4d4dff",
+    "hbond": "#ff3300",
+    "waterbridge": "#00cc7a",
+    "saltbridge": "#cc66ff",
+    "pistacking": "#ff9933",
+    "pication": "#33ccff",
+    "halogen": "#ff6699",
+    "metal": "#ace600",
+}
+
 
 def prepare_plotdata(residue_file, interaction_types, fingerprint):
     """
@@ -63,3 +74,140 @@ def fingerprint_heatmap(residue_file, interaction_types, fingerprint):
     sns.heatmap(plotdata.T, annot=True, cmap="YlGnBu", ax=ax)
     plt.xlabel("Residues")
     plt.ylabel("Interaction Types")
+
+
+def fingerprint_table(residue_file, interaction_types, count_fp, freq_fp, fp_type):
+    # , table_type
+    if fp_type == "frequency":
+        fp_table_html(residue_file, interaction_types, freq_fp)
+    else:
+        fp_table_html(residue_file, interaction_types, count_fp)
+        # round freq fp
+    # if table_type == 'full':
+    #
+    # else:
+    #
+
+
+def prepare_tabledata(residue_file, interaction_types, fingerprint):
+    """
+    Create interaction index dictionary for fingerprint table.
+    """
+    residues = read_residues(residue_file)
+    res_fp = list(divide_list(fingerprint, len(interaction_types)))
+    fp_id = range(0, len(residues) * len(interaction_types))  # all fp indices
+    interaction_list = interaction_types * len(residues)
+    interaction_index = dict(zip(fp_id, interaction_list))
+    return res_fp, interaction_index
+
+
+def cell_colour(fp_index, interaction_index):
+    interaction_type = interaction_index[fp_index]
+    interaction_colour = interaction_colours[interaction_type]
+    return interaction_colour, interaction_type
+
+
+def fp_table_html(residue_file, interaction_types, fingerprint):
+    res_fp, interaction_index = prepare_tabledata(
+        residue_file, interaction_types, fingerprint
+    )
+    html_legend = (
+        "<h3>Interactions in pre-defined binding site residues</h3><table><tr>"
+    )
+    for key in interaction_colours:
+        html_legend = (
+            html_legend
+            + '<td style="color:#fff; font-weight:bold; background-color:'
+            + interaction_colours[key]
+            + ';text-align:center">'
+            + key
+            + "</td>"
+        )
+    html_legend = html_legend + "</tr></table>"
+
+    html_str = """<html><style>
+    .ttooltip {
+    position: relative;
+    display: inline-block;
+    }
+
+    .ttooltip .ttooltiptext {
+    visibility: hidden;
+    width: 120px;
+    background-color: #000000;
+    color: #fff;
+    text-align: center;
+    padding: 5px 0;
+    border-radius: 6px;
+
+    position: absolute;
+    z-index: 1;
+    top: -5px;
+    left: 105%;
+
+    opacity: 0.3;
+    transition: opacity 0.3s;
+    }
+
+    .ttooltip .ttooltiptext::after {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: 
+    }
+
+
+    .ttooltip:hover .ttooltiptext {
+    visibility: visible;
+    opacity: 0.7;
+    }
+    </style>
+    """
+    res_fp = list(
+        divide_list(fingerprint, len(interaction_types))
+    )  # divide fp for each redisue
+    residues = read_residues(residue_file)
+    ints = len(interaction_types)
+    html_str = html_str + '<table style="border:1px solid;border-color:#9698ed"><tr>'
+    for res in residues:
+        html_str = (
+            html_str
+            + '<th style="color:#fff;border:1px solid;border-color:#9698ed;background-color:#9698ed; text-align:center">'
+            + str(res)
+            + "</th>"
+        )
+
+    html_str = html_str + "</tr><tr>"
+
+    fp_index = 0
+    for res in res_fp:
+        html_str = (
+            html_str + '<td style="border:1px solid;border-color:#9698ed;"><table><tr>'
+        )
+        for i in res:
+            if i == 0:
+                html_str = html_str + '<td style="background-color:#ffffff"></td>'
+            else:
+                interaction_colour, interaction_type = cell_colour(
+                    fp_index, interaction_index
+                )
+                html_str = (
+                    html_str
+                    + '<td style="color:#fff; font-weight:bold; background-color:'
+                    + interaction_colour
+                    + '"><div class="ttooltip">'
+                    + str(i)
+                    + '<span class="ttooltiptext">'
+                    + interaction_type
+                    + "</span></div></td>"
+                )
+            fp_index += 1
+        html_str = html_str + "</tr></table></td>"
+
+    html_str = html_str + "</tr></table></html>"
+    display(HTML(html_legend))
+    display(HTML(html_str))
