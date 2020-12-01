225a12a8fad822f4464976c9c4a65373be4bde59
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-12-01 16:18:16 +0100

    Add NGLView visualizer

--- plipify/visualization.py
+++ plipify/visualization.py
@@ -2,7 +2,8 @@
 visualization.py
 
 The visualization module for the calculated fingerprints.
-This module takes the fingerprint in dataframe, processes the data further and creates mutliple different visualizations:
+This module takes the fingerprint in dataframe, processes the data
+further and creates mutliple different visualizations:
 - Interactive stacked bar chart
 - Heatmap
 - Colour coded fingerprint table
@@ -71,7 +72,9 @@ def fingerprint_heatmap(fingerprint_df):
 def _prepare_tabledata(fingerprint_df):
     """
     Create interaction index dictionary for fingerprint table.
-    The keys of this dictionary are the interaction types and the values for each interaction type are a list of the positions (indices) of the fingerprint array that represent this interaction type.
+    The keys of this dictionary are the interaction types and the values
+    for each interaction type are a list of the positions (indices) of
+    the fingerprint array that represent this interaction type.
 
     Parameters
     ----------
@@ -88,7 +91,7 @@ def _prepare_tabledata(fingerprint_df):
     residues = list(fingerprint_df.index)
     interaction_types = list(fingerprint_df.columns)
     fingerprint = fingerprint_df.values.tolist()
-    fp_id = range(0, len(residues) * len(interaction_types))  # all fp indices
+    fp_id = range(len(residues) * len(interaction_types))  # all fp indices
     interaction_list = interaction_types * len(residues)
     interaction_index = dict(zip(fp_id, interaction_list))
     return fingerprint, interaction_index, residues
@@ -226,3 +229,106 @@ def fingerprint_table(fingerprint_df, as_widget=True):
 
         return HTML(html)
     return html
+
+
+def fingerprint_nglview(fingerprint_df, protein_path):
+    """
+    Depict interaction hotspots in NGLView
+
+    Parameters
+    ----------
+    fingerprint_df: pandas.Dataframe
+    protein_path: str or pathlib.Path
+
+    Returns
+    -------
+    view : nglview.NGLWidget
+    """
+    import nglview as nv
+
+    view = nv.NGLWidget(height="600px")
+    structure = nv.adaptor.FileStructure(str(protein_path))
+    protein_component = view.add_component(structure)
+
+    selection, selection_ons = [], []
+    tooltips = {}
+    for residue, row in fingerprint_df.iterrows():
+        resid = residue[4:].split(".")[0]
+        values = sorted(zip(fingerprint_df.columns, row), key=lambda kv: kv[1], reverse=True)
+        tooltips[resid] = ", ".join([f"{val}x{col.title()}" for (col, val) in values if val])
+
+        # Display interacting residues
+        selection.append(f"({resid} and not _H)")
+        selection_ons.append(f"({resid} and ((_O) or (_N) or (_S)))")
+
+    protein_component.add_ball_and_stick(
+        sele=" or ".join(selection), colorScheme="chainindex", aspectRatio=1.5
+    )
+    protein_component.add_ball_and_stick(
+        sele=" or ".join(selection_ons), colorScheme="element", aspectRatio=1.5
+    )
+
+    # Patch tooltips
+    # Adapted from https://github.com/umesh-timalsina/mbuild/blob/master/mbuild/utils/jsutils.py
+
+    tooltip_js = (
+        f"""
+        const extraTooltips = {tooltips};
+        """
+        # We split the fstring in two so the curly braces below do not need to escaped
+        """
+        this.stage.mouseControls.add('hoverPick', (stage, pickingProxy) => {
+            let tooltip = this.stage.tooltip;
+            if(pickingProxy && pickingProxy.atom && !pickingProxy.bond){
+                let atom = pickingProxy.atom;
+                tooltip.innerText = "ATOM: " + atom.qualifiedName();
+                if(atom.resno in extraTooltips) {
+                    tooltip.innerText += ". Residue interactions: " + extraTooltips[atom.resno];
+                }
+            }
+        });
+        """
+    )
+
+    infotext_js = (
+        f"""
+        const extraTooltips = {tooltips};
+        """
+        """
+        this.stage.signals.clicked.removeAll();
+        this.stage.signals.clicked.add((pickingProxy) => {
+                if(pickingProxy){
+                   let pickingText = null;
+                   this.model.set('picked', {});
+                   this.touch();
+                   let currentPick = {};
+                   if(pickingProxy.atom){
+                        currentPick.atom1 = pickingProxy.atom.toObject();
+                        currentPick.atom1.name = pickingProxy.atom.qualifiedName();
+                        pickingText = "Atom: " + currentPick.atom1.name;
+                        if(currentPick.atom1.resno in extraTooltips) {
+                            pickingText += ". Residue interactions: " + extraTooltips[currentPick.atom1.resno];
+                        }
+                   }
+
+                   if(pickingProxy.instance){
+                        currentPick.instance = pickingProxy.instance;
+                   }
+                   var nComponents = this.stage.compList.length;
+                   for(let i = 0; i < nComponents; i++){
+                        let comp = this.stage.compList[i];
+                        if(comp.uuid == pickingProxy.component.uuid){
+                            currentPick.component = i;
+                        }
+                   }
+                   this.model.set('picked', currentPick);
+                   this.touch();
+                   this.$pickingInfo.text(pickingText);
+                }
+        });
+        """
+    )
+    view._js(tooltip_js)
+    view._js(infotext_js)
+
+    return view
